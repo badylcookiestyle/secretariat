8b68f17884af32c5d25bcdc027325cef1c06ce92
student <student@s217-pc01>
2021-11-24 12:33:03 +0100

    Dodaj pliki projektów.

---
+++ secretary/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//przedmioty nauczane lista
+// 
+namespace secretary
+{
+    public abstract class Person
+    {
+        public string name;
+        public string secondName;
+        public string surname;
+        public string maidenName;
+        public string fathersName;
+        public string mothersName;
+        public DateTime birthDate;
+        public string[] pesel= new string[11];
+        public string imagePath;
+        public char gender;
+        
+    }
+}
